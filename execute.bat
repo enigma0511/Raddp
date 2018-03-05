db627df7dcecf4da8a1bc38c1b64e9d8900e39ab
santhoshraddp <E1>
2018-03-05 17:25:31 +0530

    initial commit

--- execute.bat
+++ execute.bat
@@ -1,14 +1,14 @@
 cd C:\Users\SA389141\Desktop\BasicExpressnode\downloads\SI273181
 git
 git init
-git config --global user.name "PriyankaRaddp"
+git config --global user.name "santhoshraddp"
 git config --global user.email "E1"
 git config --global credential.helper wincred
 git add . 
 git commit -m "initial commit"
 git remote -v
-git remote set-url origin https://PriyankaRaddp:Priya1684@github.com/PriyankaRaddp/Raddp.git
-git remote add origin https://github.com/PriyankaRaddp/Raddp.git
+git remote set-url origin https://santhoshraddp:Santhosh123@github.com/santhoshraddp/Raddp.git
+git remote add origin https://github.com/santhoshraddp/Raddp.git
 git push -u origin master 
 echo Press Enter...
 pause
